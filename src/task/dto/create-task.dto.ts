0bf4596506bab429457318fc16cb6708a8e772b6
ezqHerrera <rodherrera14@gmail.com>
2024-06-04 21:58:35 -0300

    basic validations

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateTaskDto {
+    @IsNotEmpty()
     title: string;
+
     description: string;
+
+    @IsNotEmpty()
     state: string;
 }
